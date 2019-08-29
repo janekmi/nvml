pmem/pmdk

test: fix int overflow in pmem_deep_persist test

src/test/pmem_deep_persist/pmem_deep_persist.c
@@ -69,8 +69,8 @@ main(int argc, char *argv[])
 	int ret = -1;
 
 	path = argv[1];
-	persist_size = (size_t)atoi(argv[3]);
-	offset = (size_t)atoi(argv[4]);
+	persist_size = ATOULL(argv[3]);
+	offset = ATOULL(argv[4]);
 
 	switch (*argv[2]) {
 		case 'p':

janekmi/nvml

Ref: pmem/issues#1104

src/test/pmem_deep_persist/pmem_deep_persist.c
@@ -69,8 +69,8 @@ main(int argc, char *argv[])
 	int ret = -1;
 
 	path = argv[1];
-	persist_size = (size_t)atoi(argv[3]);
-	offset = (size_t)atoi(argv[4]);
+	persist_size = ATOLL(argv[3]);
+	offset = ATOULL(argv[4]);
 
 	switch (*argv[2]) {
 		case 'p':