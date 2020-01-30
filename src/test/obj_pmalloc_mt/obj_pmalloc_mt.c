dbd6221daa70126ea4e725ca23502eec11bd365f
Jan M Michalski <jan.m.michalski@intel.com>
2020-01-30 09:13:54 +0100

    wip wip wip

--- src/test/obj_pmalloc_mt/obj_pmalloc_mt.c
+++ src/test/obj_pmalloc_mt/obj_pmalloc_mt.c
@@ -45,22 +45,23 @@
 #define MAX_THREADS 32
 #define MAX_OPS_PER_THREAD 1000
 #define ALLOC_SIZE 104
-#define REALLOC_SIZE (ALLOC_SIZE * 3)
-#define MIX_RERUNS 2
 
-#define CHUNKSIZE (1 << 18)
+#define CHUNKSIZE (1 << 20)
 #define CHUNKS_PER_THREAD 3
 
 static unsigned Threads;
 static unsigned Ops_per_thread;
 static unsigned Tx_per_thread;
 
+#define PAD_SIZE 100
+
 struct action {
 	struct pobj_action pact;
+	unsigned padding0[PAD_SIZE];
 	os_mutex_t lock;
-	unsigned padding1[10];
+	unsigned padding1[PAD_SIZE];
 	os_cond_t cond;
-	unsigned padding2[10];
+	unsigned padding2[PAD_SIZE];
 };
 
 struct root {
@@ -73,161 +74,6 @@ struct worker_args {
 	struct root *r;
 	unsigned idx;
 };
-
-static void *
-alloc_worker(void *arg)
-{
-	struct worker_args *a = arg;
-
-	for (unsigned i = 0; i < Ops_per_thread; ++i) {
-		pmalloc(a->pop, &a->r->offs[a->idx][i], ALLOC_SIZE, 0, 0);
-		UT_ASSERTne(a->r->offs[a->idx][i], 0);
-	}
-
-	return NULL;
-}
-
-static void *
-realloc_worker(void *arg)
-{
-	struct worker_args *a = arg;
-
-	for (unsigned i = 0; i < Ops_per_thread; ++i) {
-		prealloc(a->pop, &a->r->offs[a->idx][i], REALLOC_SIZE, 0, 0);
-		UT_ASSERTne(a->r->offs[a->idx][i], 0);
-	}
-
-	return NULL;
-}
-
-static void *
-free_worker(void *arg)
-{
-	struct worker_args *a = arg;
-
-	for (unsigned i = 0; i < Ops_per_thread; ++i) {
-		pfree(a->pop, &a->r->offs[a->idx][i]);
-		UT_ASSERTeq(a->r->offs[a->idx][i], 0);
-	}
-
-	return NULL;
-}
-
-static void *
-mix_worker(void *arg)
-{
-	struct worker_args *a = arg;
-
-	/*
-	 * The mix scenario is ran twice to increase the chances of run
-	 * contention.
-	 */
-	for (unsigned j = 0; j < MIX_RERUNS; ++j) {
-		for (unsigned i = 0; i < Ops_per_thread; ++i) {
-			pmalloc(a->pop, &a->r->offs[a->idx][i],
-				ALLOC_SIZE, 0, 0);
-			UT_ASSERTne(a->r->offs[a->idx][i], 0);
-		}
-
-		for (unsigned i = 0; i < Ops_per_thread; ++i) {
-			pfree(a->pop, &a->r->offs[a->idx][i]);
-			UT_ASSERTeq(a->r->offs[a->idx][i], 0);
-		}
-	}
-
-	return NULL;
-}
-
-static void *
-tx_worker(void *arg)
-{
-	struct worker_args *a = arg;
-
-	/*
-	 * Allocate objects until exhaustion, once that happens the transaction
-	 * will automatically abort and all of the objects will be freed.
-	 */
-	TX_BEGIN(a->pop) {
-		for (unsigned n = 0; ; ++n) { /* this is NOT an infinite loop */
-			pmemobj_tx_alloc(ALLOC_SIZE, a->idx);
-			if (Ops_per_thread != MAX_OPS_PER_THREAD &&
-			    n == Ops_per_thread) {
-				pmemobj_tx_abort(0);
-			}
-		}
-	} TX_END
-
-	return NULL;
-}
-
-static void *
-tx3_worker(void *arg)
-{
-	struct worker_args *a = arg;
-
-	/*
-	 * Allocate N objects, abort, repeat M times. Should reveal issues in
-	 * transaction abort handling.
-	 */
-	for (unsigned n = 0; n < Tx_per_thread; ++n) {
-		TX_BEGIN(a->pop) {
-			for (unsigned i = 0; i < Ops_per_thread; ++i) {
-				pmemobj_tx_alloc(ALLOC_SIZE, a->idx);
-			}
-			pmemobj_tx_abort(EINVAL);
-		} TX_END
-	}
-
-	return NULL;
-}
-
-static void *
-alloc_free_worker(void *arg)
-{
-	struct worker_args *a = arg;
-
-	PMEMoid oid;
-	for (unsigned i = 0; i < Ops_per_thread; ++i) {
-		int err = pmemobj_alloc(a->pop, &oid, ALLOC_SIZE,
-				0, NULL, NULL);
-		UT_ASSERTeq(err, 0);
-		pmemobj_free(&oid);
-	}
-
-	return NULL;
-}
-
-#define OPS_PER_TX 10
-#define STEP 8
-#define TEST_LANES 4
-
-static void *
-tx2_worker(void *arg)
-{
-	struct worker_args *a = arg;
-
-	for (unsigned n = 0; n < Tx_per_thread; ++n) {
-		PMEMoid oids[OPS_PER_TX];
-		TX_BEGIN(a->pop) {
-			for (int i = 0; i < OPS_PER_TX; ++i) {
-				oids[i] = pmemobj_tx_alloc(ALLOC_SIZE, a->idx);
-				for (unsigned j = 0; j < ALLOC_SIZE;
-						j += STEP) {
-					pmemobj_tx_add_range(oids[i], j, STEP);
-				}
-			}
-		} TX_END
-
-		TX_BEGIN(a->pop) {
-			for (int i = 0; i < OPS_PER_TX; ++i)
-				pmemobj_tx_free(oids[i]);
-		} TX_ONABORT {
-			UT_ASSERT(0);
-		} TX_END
-	}
-
-	return NULL;
-}
 
 static void *
 action_cancel_worker(void *arg)
@@ -252,67 +98,6 @@ action_cancel_worker(void *arg)
 			pmemobj_cancel(a->pop, &act->pact, 1);
 			util_mutex_unlock(&act->lock);
 		}
-	}
-
-	return NULL;
-}
-
-static void *
-action_publish_worker(void *arg)
-{
-	struct worker_args *a = arg;
-
-	PMEMoid oid;
-	for (unsigned i = 0; i < Ops_per_thread; ++i) {
-		unsigned arr_id = a->idx / 2;
-		struct action *act = &a->r->actions[arr_id][i];
-		if (a->idx % 2 == 0) {
-			util_mutex_lock(&act->lock);
-			oid = pmemobj_reserve(a->pop,
-				&act->pact, ALLOC_SIZE, 0);
-			UT_ASSERT(!OID_IS_NULL(oid));
-			util_cond_signal(&act->cond);
-			util_mutex_unlock(&act->lock);
-		} else {
-			util_mutex_lock(&act->lock);
-			while (act->pact.heap.offset == 0)
-				util_cond_wait(&act->cond, &act->lock);
-			pmemobj_publish(a->pop, &act->pact, 1);
-			util_mutex_unlock(&act->lock);
-		}
-	}
-
-	return NULL;
-}
-
-static void *
-action_mix_worker(void *arg)
-{
-	struct worker_args *a = arg;
-
-	PMEMoid oid;
-	for (unsigned i = 0; i < Ops_per_thread; ++i) {
-		unsigned arr_id = a->idx / 2;
-		unsigned publish = i % 2;
-		struct action *act = &a->r->actions[arr_id][i];
-		if (a->idx % 2 == 0) {
-			util_mutex_lock(&act->lock);
-			oid = pmemobj_reserve(a->pop,
-				&act->pact, ALLOC_SIZE, 0);
-			UT_ASSERT(!OID_IS_NULL(oid));
-			util_cond_signal(&act->cond);
-			util_mutex_unlock(&act->lock);
-		} else {
-			util_mutex_lock(&act->lock);
-			while (act->pact.heap.offset == 0)
-				util_cond_wait(&act->cond, &act->lock);
-			if (publish)
-				pmemobj_publish(a->pop, &act->pact, 1);
-			else
-				pmemobj_cancel(a->pop, &act->pact, 1);
-			util_mutex_unlock(&act->lock);
-		}
-		pmemobj_persist(a->pop, act, sizeof(*act));
 	}
 
 	return NULL;
@@ -402,36 +187,9 @@ main(int argc, char *argv[])
 		}
 	}
 
-	run_worker(alloc_worker, args);
-	run_worker(realloc_worker, args);
-	run_worker(free_worker, args);
-	run_worker(mix_worker, args);
-	run_worker(alloc_free_worker, args);
 	run_worker(action_cancel_worker, args);
+	sleep(5);
 	actions_clear(pop, r);
-	run_worker(action_publish_worker, args);
-	actions_clear(pop, r);
-	run_worker(action_mix_worker, args);
-
-	/*
-	 * Reduce the number of lanes to a value smaller than the number of
-	 * threads. This will ensure that at least some of the state of the lane
-	 * will be shared between threads. Doing this might reveal bugs related
-	 * to runtime race detection instrumentation.
-	 */
-	unsigned old_nlanes = pop->lanes_desc.runtime_nlanes;
-	pop->lanes_desc.runtime_nlanes = TEST_LANES;
-	run_worker(tx2_worker, args);
-	pop->lanes_desc.runtime_nlanes = old_nlanes;
-
-	/*
-	 * This workload might create many allocation classes due to pvector,
-	 * keep it last.
-	 */
-	if (Threads == MAX_THREADS) /* don't run for short tests */
-		run_worker(tx_worker, args);
-
-	run_worker(tx3_worker, args);
 
 	pmemobj_close(pop);
 
